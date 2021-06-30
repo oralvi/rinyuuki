ec9f8fe81d289a8be34ab539c304b648c6ea6336
Oralvi <sakura.oralvi@gmail.com>
2021-06-30 12:06:21 +0800

    0.0.1

--- rin/modules/groupmaster/get_cqcode.py
+++
@@ -1,7 +0,0 @@
-from rin import sucmd
-from rin.typing import CommandSession
-from rin.util import escape
-
-@sucmd('取码', force_private=False)
-async def get_cqcode(session: CommandSession):
-    await session.send(escape(str(session.current_arg)))
--- run.py
+++
@@ -1,8 +0,0 @@
-import rin
-import asyncio
-
-bot = rin.init()
-app = bot.asgi
-
-if __name__ == '__main__':
-    bot.run(use_reloader=False, loop=asyncio.get_event_loop())
