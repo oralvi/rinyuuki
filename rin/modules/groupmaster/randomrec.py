c4929e8a66d9e5601ee4b5ac5fae07a5c8cbb39d
Oralvi <sakura.oralvi@gmail.com>
2021-06-30 11:42:29 +0800

    +炎王的美妙歌喉

--- rin/modules/groupmaster/randomrec.py
+++ rin/modules/groupmaster/randomrec.py
@@ -11,3 +11,6 @@ sv = Service('randomrec', visible=True)
 @sv.on_keyword('xcw')
 async def voicetest(bot, ev):
     await bot.send(ev, R.rec('test.silk').cqcode)
+@sv.on_keyword('炎王的美妙歌喉')
+async def voicetest(bot, ev):
+    await bot.send(ev, R.rec('test1.silk').cqcode)
