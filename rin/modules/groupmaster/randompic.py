9eabd9c69c1826939c3df4797c255de3bc0b66fc
Oralvi <sakura.oralvi@gmail.com>
2021-08-20 13:05:31 +0800

    修复randompic爆率

--- rin/modules/groupmaster/randompic.py
+++ rin/modules/groupmaster/randompic.py
@@ -10,14 +10,14 @@ sv = Service('randompic', visible=True)
 
 @sv.on_keyword(('确实', '有一说一', 'u1s1', 'yysy'))
 async def chat_queshi(bot, ctx):
-    if random.random() < 0.05:
+    if random.random() < 0.10:
         await bot.send(ctx, R.img(f"确实{random.randint(1, 2)}.jpg").cqcode)
 
 
 @sv.on_keyword(('会战'))
 async def chat_clanba(bot, ctx):
-    if random.random() < 0.02:
-        await bot.send(ctx, R.img('我的天啊你看看都几度了.jpg').cqcode)
+    if random.random() < 0.10:
+        await bot.send(ctx, R.img('我的天啊你看看都几点了.jpg').cqcode)
 
 
 @sv.on_keyword(('内鬼'))
@@ -27,48 +27,44 @@ async def chat_neigui(bot, ctx):
 
 nyb_player = f'''{R.img('nyb.gif').cqcode}
 正在播放：New Year Burst
-──●━━━━ 1:05/1:30
+──●━━━━ 1:10/1:30
 ⇆ ㅤ◁ ㅤㅤ❚❚ ㅤㅤ▷ ㅤ↻
 '''.strip()
 
 @sv.on_keyword(('春黑', '新黑'))
 async def new_year_burst(bot, ev):
-    if random.random() < 0.05:
+    if random.random() < 0.10:
         await bot.send(ev, nyb_player)
 
 @sv.on_keyword(('露娜', 'luna'))
-async def new_year_burst(bot, ctx):
-    if random.random() < 0.05:
+async def luna(bot, ctx):
+    if random.random() < 0.10:
         await bot.send(ctx, R.img('luna.gif').cqcode)
 
 @sv.on_keyword(('就这'))
-async def new_year_burst(bot, ctx):
-    if random.random() < 0.05:
+async def jiuzhe(bot, ctx):
+    if random.random() < 0.10:
         await bot.send(ctx, R.img('就这.png').cqcode)
 
-@sv.on_keyword(('u1s1', '有一说一'))
-async def new_year_burst(bot, ctx):
-    if random.random() < 0.05:
-        await bot.send(ctx, R.img('确实2.jpg').cqcode)
 
 @sv.on_keyword(('绝了'))
-async def new_year_burst(bot, ctx):
-    if random.random() < 0.05:
+async def juele(bot, ctx):
+    if random.random() < 0.10:
         await bot.send(ctx, R.img('绝了.jpg').cqcode)
 
 @sv.on_keyword(('啊这', 'az'))
-async def new_year_burst(bot, ctx):
-    if random.random() < 0.05:
+async def az(bot, ctx):
+    if random.random() < 0.10:
         await bot.send(ctx, R.img('啊这.jpg').cqcode)
 
 @sv.on_keyword(('laopo', 'lp', '老婆'))
-async def new_year_burst(bot, ctx):
-    if random.random() < 0.05:
+async def hanlaopo(bot, ctx):
+    if random.random() < 0.10:
         await bot.send(ctx, R.img(f"laopo{random.randint(1, 2)}.jpg").cqcode)
 
 @sv.on_keyword(('se', '涩', '色图'))
-async def new_year_burst(bot, ctx):
-    if random.random() < 0.05:
-        await bot.send(ctx, R.img('涩图审核.jpg').cqcode)
+async def buzhunkansetu(bot, ctx):
+    if random.random() < 0.10:
+        await bot.send(ctx, R.img(f"涩图审核{random.randint(1, 2)}.jpg").cqcode)
 
 
